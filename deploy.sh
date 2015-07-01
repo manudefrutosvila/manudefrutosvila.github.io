bdbd1c036f2963c647d004b39118294b9d8ded2e
Manu de Frutos Vila <manuel.defrutosvila@sopra.com>
2015-07-01 16:31:49 +0200

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Deploy script thanks to https://github.com/PascalPrecht
+
+echo -e "\033[0;32mDeploying new manudefrutosvila.github.io site...\033[0m"
+
+# delete old gh-pages branch
+git branch -D deploy
+
+git checkout -b deploy
+
+# Add changes to git.
+git add -f dist
+
+# Commit changes.
+msg="chore(*): adding distribution `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git subtree split -P dist -b deploy-dist
+
+# Push source and build repos.
+git push -f upstream deploy-dist:master
+git branch -D deploy-dist
+git checkout dev
+git branch -D deploy
